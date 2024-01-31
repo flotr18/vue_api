0e209f8f53e1661149cd50293eb860dc5127b29b
flotr18 <55090943+flotr18@users.noreply.github.com>
2024-01-31 16:14:36 +0100

    Create f1_view.vue

---
+++ src/views/f1_view.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'f1_view',
+    data() {
+      return {}
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
