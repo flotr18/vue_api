933f470ae47a5979463e37cc2c6fdfdcdfe93d37
flotr18 <55090943+flotr18@users.noreply.github.com>
2024-01-31 16:10:32 +0100

    Init Repo

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Bonjour Voici ma About View
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AboutView',
+    data() {
+      return {}
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Bonjour voici ma HomePage
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeView',
+    data() {
+      return {}
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
